c559f696c6242516976dd8d95bf8e598a7d3481f
Sean Harte <scsharte@gmail.com>
2018-05-26 11:08:14 -0400

    github aurh get routers

---
+++ users/router.js
@@ -0,0 +1,24 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const passportGithub = require('../auth/strategies');
+
+const router = express.Router();
+
+
+//GET /auth/github
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in GitHub authentication will involve redirecting
+//   the user to github.com.  After authorization, GitHub will redirect the user
+//   back to this application at /auth/github/callback
+router.get('/auth/github', passportGithub.authenticate('github', { scope: [ 'user:email' ] }));
+
+router.get('/auth/github/callback',
+  passportGithub.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication
+    res.json(req.user);
+  });
+
+module.exports = router;
