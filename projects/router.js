d87235f6ceb02aa1435d36896abc85a826e0e58f
Sean Harte <scsharte@gmail.com>
2018-05-26 16:54:52 -0400

    correct get by owner id

--- projects/router.js
+++ projects/router.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { Projects } = require('./models');
+
 //*** get All projects ***
 router.get('/', (req, res, next) => {
 	Projects.find()
