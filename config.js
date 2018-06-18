5750a57ab845030cdac6d5b6c4f0abb25c8c3926
Sean Harte <scsharte@gmail.com>
2018-06-17 21:09:29 -0400

    corrected github id & secret

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
-exports.GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || "f84675d1-22c2-4508-9864-c61f3d591ef8";
-exports.GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET ||	"85fc6d0c2195ca7b7e18f33eae14f8858a2074d89c1420992361b89a575a74a2";
+exports.GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+exports.GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 //connects to the database 
 exports.DATABASE_URL =  process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/collab-board'  ;
